654c85b667cc664c13492e703b59537009ead77e
Liam <liampacker@gmail.com>
2022-05-15 11:44:26 -0400

    now can successfully parse on the spec, but not written tail-recursively so overflows on slippi replays

--- lib/ubjson_parser.ml
+++ lib/ubjson_parser.ml
@@ -24,57 +24,66 @@ type ubjson =
   | `Object of (string * ubjson) list
   | `Array  of ubjson list ];;
 
+let take1 = take 1 >>= fun x -> return (String.get x 0)
+let peek1 = peek_char_fail >>= fun x -> return x
 
 (* Number parsers *)
 module N = struct
-  let _uint8 : ubjson t =
-    let open Bytes in
-    char 'U' *> take 1 >>= fun x -> return (`UInt8 (get_uint8 (of_string x) 0))
+  let parse_number i f =
+    take i >>= fun x -> if (String.equal x "]" || String.equal x "}") then fail "parse_number" else return (f x)
+
+  let _uint8 =
+    parse_number  1 (fun x -> `UInt8 (Bytes.get_uint8 (Bytes.of_string x) 0))
 
   let _int8 : ubjson t =
-    let open Bytes in
-    char 'i' *> take 1 >>= fun x -> return (`Int8 (get_int8 (of_string x) 0))
+    parse_number  1 (fun x ->  `Int8 (Bytes.get_int8 (Bytes.of_string x) 0))
 
   let _int16 : ubjson t =
-    let open Bytes in
-    char 'I' *> take 2 >>= fun x -> return (`Int16 (get_int16_be (of_string x) 0))
+    parse_number   2 (fun x ->  `Int16 (Bytes.get_int16_be (Bytes.of_string x) 0))
 
   let _int32 : ubjson t =
-    let open Bytes in
-    char 'l' *> take 4 >>= fun x -> return (`Int32 (get_int32_be (of_string x) 0))
+    parse_number  4 (fun x -> `Int32 (Bytes.get_int32_be (Bytes.of_string x) 0))
 
   let _int64 : ubjson t =
-    let open Bytes in
-    char 'L' *> take 8 >>= fun x -> return (`Int64 (get_int64_be (of_string x) 0))
+    parse_number  8 (fun x -> `Int64 (Bytes.get_int64_be (Bytes.of_string x) 0))
 
   let _float32 : ubjson t  =
-    let open Bytes in
-    char 'd' *> take 4 >>= fun x -> return (`Float32 (get_int32_be (of_string x) 0 |> Int32.float_of_bits))
+    parse_number  4 (fun x -> `Float32 (Bytes.get_int32_be (Bytes.of_string x) 0 |> Int32.float_of_bits))
 
-  let _float64 : ubjson t =
-    let open Bytes in
-    char 'D' *> take 8 >>= fun x -> return (`Float64 (get_int64_be (of_string x) 0 |> Int64.float_of_bits))
+  let _float64 : ubjson t  =
+    parse_number  8 (fun x -> `Float32 (Bytes.get_int64_be (Bytes.of_string x) 0 |> Int64.float_of_bits))
 
-  let _number : char -> ubjson t = function
-    | 'U' -> _uint8
-    | 'i' -> _int8
-    | 'I' -> _int16
-    | 'l' -> _int32
-    | 'L' -> _int64
-    | 'd' -> _float32
-    | 'D' -> _float64
-    | x -> fail ("_number Failed on char " ^ (String.make 1 x))
+  let _to_value x =
+    match x with
+    | `UInt8 y -> y
+    | `Int8 y -> y
+    | `Int16 y -> y
+    | `Int32 y -> (Int32.to_int y)
+    | `Int64 y -> (Int64.to_int y)
+    | _ -> assert false
+
+  let _number c =
+    char c *>
+      (match c with
+       | 'U' -> _uint8
+       | 'i' -> _int8
+       | 'I' -> _int16
+       | 'l' -> _int32
+       | 'L' -> _int64
+       | 'd' -> _float32
+       | 'D' -> _float64
+       | x -> fail ("_number Failed on char " ^ (String.make 1 x)))
 
 end
 
-let _false : ubjson t = char 'F' *> return `False
-let _true  : ubjson t = char 'T'  *> return `True
-let _null  : ubjson t = char 'Z'  *> return `Null
-let _no_op  : ubjson t = char 'N'  *> return `NoOp
+let _false : ubjson t = return `False
+let _true  : ubjson t = return `True
+let _null  : ubjson t = return `Null
+let _no_op  : ubjson t = return `NoOp
 
 let _char : ubjson t =
   let open Bytes in
-  char 'C' *> take 1 >>= fun x -> return (`Char (String.get (to_string (of_string x)) 0))
+  take1 >>= fun x -> return (`Char x)
 
 let _id_string : string t =
   peek_char_fail >>= fun x -> N._number x
@@ -83,51 +92,71 @@ let _id_string : string t =
                | `Int16 y -> take y
                | `Int32 y -> take (Int32.to_int y)
                | `Int64 y -> take (Int64.to_int y)
-               | _ -> fail "HELP"
+               | _ -> fail "Identifier String"
 
 let _string : ubjson t =
   let open Bytes in
-  char 'S' *> _id_string >>= fun s -> return (`String s)
+  _id_string >>= fun s -> return (`String s)
+
+let _parse_char : char -> ubjson t =
+  function c ->
+            match c with
+            | 'U' -> N._uint8
+            | 'i' -> N._int8
+            | 'I' -> N._int16
+            | 'l' -> N._int32
+            | 'L' -> N._int64
+            | 'd' -> N._float32
+            | 'D' -> N._float64
+            | 'F' ->  _false
+            | 'T' ->  _true
+            | 'Z' ->  _null
+            | 'N' ->  _no_op
+            | 'C' ->  _char
+            | 'S' ->  _string
+            | c -> fail ("End of Grammar: "^(String.make 1 c))
 
 
+(* TODO: not tail-recursive, also ugly *)
 let ubjson =
   let pair x y = (x,y) in
   fix (fun ubjson ->
       let mem = lift2 pair _id_string ubjson in
-      let obj = char '{'
-                *> many mem <* char '}' >>= fun ms -> return (`Object ms) in
-      let arr = char '['
-                *> many ubjson <* char ']' >>= fun ms -> return (`Array ms) in
+      let mem_fixed_type t = lift2 pair _id_string t in
+      (* can do `many _type` maybe *)
+      let maybe_type =
+        char '$' *> take1 in
+      (* can do `repeat n ubjson` or `repeat n _type` *)
+      (* just don't allow the container types to be an allowable _type lol *)
+      let maybe_length =
+        char '#' *> peek1 >>= N._number in
+      let obj =
+        ((lift2 pair maybe_type maybe_length
+          >>= fun x -> let c,l = x in
+                       count (N._to_value l) (mem_fixed_type (_parse_char c))) <|>
+           (maybe_length
+            >>= fun l ->
+            count (N._to_value l) mem) <|>
+           (maybe_type >>= fun c -> many (mem_fixed_type (_parse_char c)) <* char '}') <|>
+           (many mem <* char '}'))
+        >>= fun ms -> return (`Object ms) in
+      let arr =
+        ((lift2 pair maybe_type maybe_length
+          >>= fun x -> let c,l = x in
+                       count (N._to_value l) (_parse_char c)) <|>
+           (maybe_length
+            >>= fun l ->
+            count (N._to_value l) ubjson) <|>
+           (maybe_type >>= fun c -> many (_parse_char c) <* char ']') <|>
+           (many ubjson <* char ']'))
+        >>= fun ms -> return (`Array ms) in
       peek_char_fail
-      >>= function
-      | 'F' -> _false | 'T' -> _true | 'Z' -> _null | 'N' -> _no_op
-      | 'U' -> N._uint8
-      | 'i' -> N._int8
-      | 'I' -> N._int16
-      | 'l' -> N._int32
-      | 'L' -> N._int64
-      | 'd' -> N._float32
-      | 'D' -> N._float64
-      | 'C' -> _char
-      | 'S' -> _string
-      | '{' -> obj
-      | '[' -> arr
-      | c -> fail ("End of Grammar: "^(String.make 1 c))
-    ) <?> "ubjson"
+      >>= function c ->   (advance 1 *> (_parse_char c)) <|>
+                            (char '{' *> obj     )<|>
+                            (char '[' *> arr     )<|>
+                            fail ("End of Grammar: "^(String.make 1 c))) <?> "ubjson"
 
-
-let read_whole_file filename =
-  let ch = open_in filename in
-  let s = really_input_string ch (in_channel_length ch) in
-  close_in ch;
-  s
-
-
-let test_str = "{i\005loginSi\007octocati\002idSi\006Mediumi\004plan{i\004nameSi\006Medium}U\003raw[U\003]}"
-
-
-let x =
-  let fn = (read_whole_file "../test/raw.slp") in
-  match (parse_string ~consume:All ubjson test_str) with
+let _ =
+  match (parse_string ~consume:All ubjson "{$U#i\003i\003lat\006i\003lon\009i\003alt\012") with
   | Ok c -> c
   | Error msg -> print_endline msg; `Null
--- test/run_tests.ml
+++ test/run_tests.ml
@@ -1,6 +1,17 @@
 open Ubjson_parser
 open Angstrom
 
+let test_easy = "[U\003d\001\002\003\004TFZNC3]"
+let test_str = "{i\005loginSi\007octocati\002idSi\006Mediumi\004plan{i\004nameSi\006Medium}U\003raw[U\003d\001\002\003\004TFZNC3]}"
+
+let test_arr_fixed_type = "[$U\000\001\002\003]"
+let test_arr_fixed_length = "[#i\006U\000U\001U\002U\003U\004U\005"
+let test_arr_fixed_type_and_length = "[$U#i\006\000\001\002\003\004\005"
+let test_obj_fixed_type = "{$Ui\003lat\003i\003lon\005i\003alt\009}"
+let test_obj_fixed_length = "{#i\003i\003latU\003i\003lonU\005i\003altU\009"
+let test_obj_fixed_type_and_length = "{$U#i\003i\003lat\003i\003lon\005i\003alt\009"
+
+
 let read_whole_file filename =
   let ch = open_in filename in
   let s = really_input_string ch (in_channel_length ch) in
