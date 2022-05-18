9c63c150f6e671729f44eaae48185909280eac0a
Liam <liampacker@gmail.com>
2022-05-17 21:38:22 -0400

    formatting, add test suite, fix `count` to be tail recursive. fixed array length untyped processing is broken for now.

--- lib/ubjson_parser.ml
+++ lib/ubjson_parser.ml
@@ -3,7 +3,6 @@ open Angstrom
 (* https://ubjson.org/ *)
 (* https://ubjson.org/type-reference/ *)
 type ubjson =
-  (* value types *)
   [ `Null
   | `NoOp
   | `True
@@ -18,140 +17,161 @@ type ubjson =
   | `HighPrecisionNumber of float
   | `Char of char
   | `String of string
-  (* https://ubjson.org/type-reference/container-types/ *)
   | `Object of (string * ubjson) list
-  | `Array  of ubjson list ];;
+  | `Array of ubjson list
+  ]
 
 let take1 = take 1 >>= fun x -> return (String.get x 0)
+
 let peek1 = peek_char_fail >>= fun x -> return x
 
 (* Number parsers *)
 module N = struct
-  let parse_number i f =
-    take i >>= fun x -> if (String.equal x "]" || String.equal x "}") then fail "parse_number" else return (f x)
+  let parse_number i f = take i >>= fun x -> return (f x)
 
-  let _uint8 =
-    parse_number  1 (fun x -> `UInt8 (Bytes.get_uint8 (Bytes.of_string x) 0))
+  let _uint8 = parse_number 1 (fun x -> `UInt8 (Bytes.get_uint8 (Bytes.of_string x) 0))
 
   let _int8 : ubjson t =
-    parse_number  1 (fun x ->  `Int8 (Bytes.get_int8 (Bytes.of_string x) 0))
+    parse_number 1 (fun x -> `Int8 (Bytes.get_int8 (Bytes.of_string x) 0))
+
 
   let _int16 : ubjson t =
-    parse_number  2 (fun x ->  `Int16 (Bytes.get_int16_be (Bytes.of_string x) 0))
+    parse_number 2 (fun x -> `Int16 (Bytes.get_int16_be (Bytes.of_string x) 0))
+
 
   let _int32 : ubjson t =
-    parse_number  4 (fun x -> `Int32 (Bytes.get_int32_be (Bytes.of_string x) 0))
+    parse_number 4 (fun x -> `Int32 (Bytes.get_int32_be (Bytes.of_string x) 0))
+
 
   let _int64 : ubjson t =
-    parse_number  8 (fun x -> `Int64 (Bytes.get_int64_be (Bytes.of_string x) 0))
+    parse_number 8 (fun x -> `Int64 (Bytes.get_int64_be (Bytes.of_string x) 0))
 
-  let _float32 : ubjson t  =
-    parse_number  4 (fun x -> `Float32 (Bytes.get_int32_be (Bytes.of_string x) 0 |> Int32.float_of_bits))
 
-  let _float64 : ubjson t  =
-    parse_number  8 (fun x -> `Float32 (Bytes.get_int64_be (Bytes.of_string x) 0 |> Int64.float_of_bits))
+  let _float32 : ubjson t =
+    parse_number 4 (fun x ->
+        `Float32 (Bytes.get_int32_be (Bytes.of_string x) 0 |> Int32.float_of_bits))
 
-  let _to_value x =
+
+  let _float64 : ubjson t =
+    parse_number 8 (fun x ->
+        `Float64 (Bytes.get_int64_be (Bytes.of_string x) 0 |> Int64.float_of_bits))
+
+
+  let _to_int_value (x : ubjson) : int =
     match x with
     | `UInt8 y -> y
-    | `Int8 y -> y
+    | `Int8 y  -> y
     | `Int16 y -> y
-    | `Int32 y -> (Int32.to_int y)
-    | `Int64 y -> (Int64.to_int y)
-    | _ -> assert false
+    | `Int32 y -> Int32.to_int y
+    | `Int64 y -> Int64.to_int y
+    | _        -> assert false
+
 
   let _number c =
-    char c *>
-      (match c with
-       | 'U' -> _uint8
-       | 'i' -> _int8
-       | 'I' -> _int16
-       | 'l' -> _int32
-       | 'L' -> _int64
-       | 'd' -> _float32
-       | 'D' -> _float64
-       | x -> fail ("_number Failed on char " ^ (String.make 1 x)))
-
+    advance 1
+    *>
+    match c with
+    | 'U' -> _uint8
+    | 'i' -> _int8
+    | 'I' -> _int16
+    | 'l' -> _int32
+    | 'L' -> _int64
+    | 'd' -> _float32
+    | 'D' -> _float64
+    | x   -> fail ("_number unrecognized char " ^ String.make 1 x)
 end
 
 let _false : ubjson t = return `False
-let _true  : ubjson t = return `True
-let _null  : ubjson t = return `Null
-let _no_op  : ubjson t = return `NoOp
 
-let _char : ubjson t =
-  take1 >>= fun x -> return (`Char x)
+let _true : ubjson t = return `True
+
+let _null : ubjson t = return `Null
+
+let _no_op : ubjson t = return `NoOp
+
+let _char : ubjson t = take1 >>= fun x -> return (`Char x)
 
 let _id_string : string t =
-  peek_char_fail >>= fun x -> N._number x
-  >>= function | `UInt8 y -> take y
-               | `Int8 y -> take y
-               | `Int16 y -> take y
-               | `Int32 y -> take (Int32.to_int y)
-               | `Int64 y -> take (Int64.to_int y)
-               | _ -> fail "Identifier String"
+  peek_char_fail
+  >>= fun x ->
+  N._number x
+  >>= function
+  | `UInt8 y -> take y
+  | `Int8 y  -> take y
+  | `Int16 y -> take y
+  | `Int32 y -> take (Int32.to_int y)
+  | `Int64 y -> take (Int64.to_int y)
+  | _        -> fail "Identifier String"
 
-let _string : ubjson t =
-  _id_string >>= fun s -> return (`String s)
 
-let _parse_char : char -> ubjson t =
-  function c ->
-            match c with
-            | 'U' -> N._uint8
-            | 'i' -> N._int8
-            | 'I' -> N._int16
-            | 'l' -> N._int32
-            | 'L' -> N._int64
-            | 'd' -> N._float32
-            | 'D' -> N._float64
-            | 'F' ->  _false
-            | 'T' ->  _true
-            | 'Z' ->  _null
-            | 'N' ->  _no_op
-            | 'C' ->  _char
-            | 'S' ->  _string
-            | c -> fail ("End of Grammar: "^(String.make 1 c))
+let _string : ubjson t = _id_string >>= fun s -> return (`String s)
+
+let _parse_char (c : char) : ubjson t =
+  match c with
+  | 'U' -> N._uint8
+  | 'i' -> N._int8
+  | 'I' -> N._int16
+  | 'l' -> N._int32
+  | 'L' -> N._int64
+  | 'd' -> N._float32
+  | 'D' -> N._float64
+  | 'F' -> _false
+  | 'T' -> _true
+  | 'Z' -> _null
+  | 'N' -> _no_op
+  | 'C' -> _char
+  | 'S' -> _string
+  | c   -> fail ("End of Grammar: " ^ String.make 1 c)
+
+
+(* A tail-recursive implementation of `count` *)
+let count_tr n p =
+  if n < 0
+  then fail "count: n < 0"
+  else (
+    let cons x xs = x :: xs in
+    let rec loop acc = function
+      | 0 -> acc
+      | n -> loop (lift2 cons p acc) (n - 1)
+    in
+    loop (return []) n)
 
 
 (* TODO: not tail-recursive, also ugly *)
 let ubjson =
-  let pair x y = (x,y) in
-  fix (fun ubjson ->
-      let mem = both _id_string ubjson in
-      let mem_fixed_type t = lift2 pair _id_string t in
-      let maybe_type =
-        char '$' *> take1 in
-      let maybe_length =
-        char '#' *> peek1 >>= N._number in
-      (* TODO: replace the "or" (<|>) parser branches with a `both (option maybe_type)
-         (option maybe_length) >>= fun x -> match x ... *)
-      let obj =
-        ((lift2 pair maybe_type maybe_length
-          >>= fun x -> let c,l = x in
-                       count (N._to_value l) (mem_fixed_type (_parse_char c))) <|>
-           (maybe_length
-            >>= fun l ->
-            count (N._to_value l) mem) <|>
-           (maybe_type >>= fun c -> many (mem_fixed_type (_parse_char c)) <* char '}') <|>
-           (many mem <* char '}'))
-        >>= fun ms -> return (`Object ms) in
-      let arr =
-        ((lift2 pair maybe_type maybe_length
-          >>= fun x -> let c,l = x in
-                       count (N._to_value l) (_parse_char c)) <|>
-           (maybe_length
-            >>= fun l ->
-            count (N._to_value l) ubjson) <|>
-           (maybe_type >>= fun c -> many (_parse_char c) <* char ']') <|>
-           (many ubjson <* char ']'))
-        >>= fun ms -> return (`Array ms) in
-      peek_char_fail
-      >>= function c ->   (advance 1 *> (_parse_char c)) <|>
-                            (char '{' *> obj     )<|>
-                            (char '[' *> arr     )<|>
-                            fail ("End of Grammar: "^(String.make 1 c))) <?> "ubjson"
-
-let _ =
-  match (parse_string ~consume:All ubjson "{i\005loginSi\007octocati\002idSi\006Mediumi\004plan{i\004nameSi\006Medium}U\003raw[U\003d\001\002\003\004TFZNC3]}") with
-  | Ok c -> c
-  | Error msg -> print_endline msg; `Null
+  let pair x y = x, y in
+  let f ubjson =
+    let mem = both _id_string ubjson in
+    let mem_fixed_type t = lift2 pair _id_string t in
+    let maybe_type = char '$' *> take1 in
+    let maybe_length = char '#' *> peek1 >>= N._number in
+    (* TODO: replace the "or" (<|>) parser branches with a `both (option maybe_type)
+       (option maybe_length) >>= fun x -> match x ... *)
+    let obj =
+      lift2 pair maybe_type maybe_length
+      >>= (fun x ->
+            let c, l = x in
+            print_endline (String.make 1 c);
+            count_tr (N._to_int_value l) (mem_fixed_type (_parse_char c)))
+      <|> (maybe_length >>= fun l -> count_tr (N._to_int_value l) mem)
+      <|> (maybe_type >>= fun c -> many (mem_fixed_type (_parse_char c)) <* char '}')
+      <|> (many mem <* char '}')
+      <?> "object"
+      >>= fun ms -> return (`Object ms)
+    in
+    let arr =
+      lift2 pair maybe_type maybe_length
+      >>= (fun x ->
+            let c, l = x in
+            count_tr (N._to_int_value l) (_parse_char c))
+      <|> (maybe_length >>= fun l -> count_tr (N._to_int_value l) ubjson)
+      <|> (maybe_type >>= fun c -> many (_parse_char c) <* char ']')
+      <|> (many ubjson <* char ']')
+      <?> "array"
+      >>= fun ms -> return (`Array ms)
+    in
+    peek_char_fail
+    >>= function
+    | c -> advance 1 *> _parse_char c <|> char '{' *> obj <|> char '[' *> arr
+  in
+  fix f <?> "ubjson"
--- test/run_tests.ml
+++ test/run_tests.ml
@@ -1,26 +1,60 @@
 open Ubjson_parser
 open Angstrom
+open OUnit2
 
-(* let test_easy = "[U\003d\001\002\003\004TFZNC3]" *)
-let test_str = "{i\005loginSi\007octocati\002idSi\006Mediumi\004plan{i\004nameSi\006Medium}U\003raw[U\003d\001\002\003\004TFZNC3]i\003obj{$Ui\003lat\003i\003lon\005i\003alt\009}i\003arr[$U#i\006\000\001\002\003\004\005}"
+let test_easy = "[U\003d\001\002\003\004TFZNC3]"
 
-(* let test_arr_fixed_type = "[$U\000\001\002\003]" *)
-(* let test_arr_fixed_length = "[#i\006U\000U\001U\002U\003U\004U\005" *)
-(* let test_arr_fixed_type_and_length = "[$U#i\006\000\001\002\003\004\005" *)
-(* let test_obj_fixed_type = "{$Ui\003lat\003i\003lon\005i\003alt\009}" *)
-(* let test_obj_fixed_length = "{#i\003i\003latU\003i\003lonU\005i\003altU\009" *)
-(* let test_obj_fixed_type_and_length = "{$U#i\003i\003lat\003i\003lon\005i\003alt\009" *)
+let test_arr_fixed_type = "[$U\000\001\002\003]"
+
+let test_arr_fixed_length = "[#i\006U\000U\001U\002U\003U\004U\005"
+
+let test_arr_fixed_type_and_length = "[$U#i\006\000\001\002\003\004\005"
+
+let test_obj_fixed_type = "{$Ui\003lat\003i\003lon\005i\003alt\009}"
+
+let test_obj_fixed_length = "{#i\003i\003latU\003i\003lonU\005i\003altU\009"
+
+let test_obj_fixed_type_and_length = "{$U#i\003i\003lat\003i\003lon\005i\003alt\009"
+
+let read_whole_file filename =
+  let ch = open_in filename in
+  let s = really_input_string ch (in_channel_length ch) in
+  close_in ch;
+  s
 
 
-(* let read_whole_file filename = *)
-(*   let ch = open_in filename in *)
-(*   let s = really_input_string ch (in_channel_length ch) in *)
-(*   close_in ch; *)
-(*   s *)
+let check_ok s =
+  match parse_string ~consume:All ubjson s with
+  | Ok _    -> true
+  | Error _ -> false
 
 
-let _ =
-  print_endline test_str;
-  match (parse_string ~consume:All ubjson test_str) with
-  | Ok c -> c
-  | Error msg -> failwith msg
+let assert_true v = assert_equal true v
+
+let test_suite =
+  "test suite ubjson_parser"
+  >::: [ ("Complete Example"
+         >:: fun _ ->
+         assert_true
+           (check_ok
+              "{i\005loginSi\007octocati\002idSi\006Mediumi\004plan{i\004nameSi\006Medium}U\003raw[U\003d\001\002\003\004TFZNC3]}")
+         )
+       ; ("Large Raw Array, length+type specified"
+         >:: fun _ ->
+         let fn = read_whole_file "../../../test/test.slp" in
+         check_ok fn |> assert_true)
+       ; ("Easy Array, all types" >:: fun _ -> check_ok test_easy |> assert_true)
+       ; ("Fixed size object" >:: fun _ -> check_ok test_obj_fixed_length |> assert_true)
+       ; ("Fixed type object" >:: fun _ -> check_ok test_obj_fixed_type |> assert_true)
+       ; ("Fixed type and size object"
+         >:: fun _ -> check_ok test_obj_fixed_type_and_length |> assert_true)
+       ; ("Fixed size array" >:: fun _ -> check_ok test_arr_fixed_length |> assert_true)
+       ; ("Fixed type array" >:: fun _ -> check_ok test_arr_fixed_type |> assert_true)
+       ; ("Fixed type and size array"
+         >:: fun _ -> check_ok test_arr_fixed_type_and_length |> assert_true)
+       ]
+
+
+let () =
+  print_endline "---------- Unit Tests ----------";
+  OUnit2.run_test_tt_main test_suite
